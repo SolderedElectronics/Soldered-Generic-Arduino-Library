186c140dd5ec563f1a1cac9fe4bb90836c656f06
Docs Robot <robot@example.com>
2022-02-17 12:00:59 +0000

    Committing documentation changes

--- docs/search/searchdata.js
+++ docs/search/searchdata.js
@@ -2,32 +2,29 @@ var indexSectionsWithContent =
 {
   0: "abdeginprs",
   1: "es",
-  2: "e",
-  3: "egr",
-  4: "beirs",
-  5: "abdenp",
-  6: "n"
+  2: "egr",
+  3: "beirs",
+  4: "abdenp",
+  5: "n"
 };
 
 var indexSectionNames =
 {
   0: "all",
   1: "classes",
-  2: "namespaces",
-  3: "files",
-  4: "functions",
-  5: "variables",
-  6: "pages"
+  2: "files",
+  3: "functions",
+  4: "variables",
+  5: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
   1: "Classes",
-  2: "Namespaces",
-  3: "Files",
-  4: "Functions",
-  5: "Variables",
-  6: "Pages"
+  2: "Files",
+  3: "Functions",
+  4: "Variables",
+  5: "Pages"
 };
 
